34657f9c9e2312e6f0a902fbcc3607283b273a1d
Fran Zekan <zekan.fran369@gmail.com>
2020-06-05 23:59:37 +0200

    Add better error handling in Home.jsx

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -1,15 +1,15 @@
 import React, { Component, Fragment } from 'react';
 import axios from 'axios';
-import config from '../config'
+import config from '../config';
 // import Card from '../components/Card'
 import TodoCard from '../components/TodoCard';
-import CreateTodoButton from '../components/CreateTodoButton'
+import CreateTodoButton from '../components/CreateTodoButton';
 
 import BottomNav from '../components/BottomNav';
 
 var api_url = config[process.env.NODE_ENV || 'development'].api_url;
 const url = document.location.href;
-if(url.indexOf(':') !== -1 && url.split('//')[1].split(':')[0].split('.').length === 4){
+if (url.indexOf(':') !== -1 && url.split('//')[1].split(':')[0].split('.').length === 4) {
   api_url = `${url.split(':').splice(0, 2).join(':')}:5000/api`;
 }
 
@@ -20,46 +20,49 @@ class Home extends Component {
     this.state = {
       todos: [],
       show_done: false,
-      loading: true
+      loading: true,
     };
 
-    this.delete = this.delete.bind(this)
-    this.check = this.check.bind(this)
-    this.changeShow = this.changeShow.bind(this)
+    this.delete = this.delete.bind(this);
+    this.check = this.check.bind(this);
+    this.changeShow = this.changeShow.bind(this);
   }
 
-  delete(id){
-    axios.delete(`${api_url}/todo/${id}`).then(res => {
-      if(res.status === 204){
+  delete(id) {
+    axios.delete(`${api_url}/todo/${id}`).then((res) => {
+      if (res.status === 204) {
         const newArray = this.state.todos.filter((todo) => todo.id !== id);
-        this.setState({todos: newArray})
+        this.setState({ todos: newArray });
       }
     });
   }
 
-  check(id, index){
-    axios.patch(`${api_url}/todo/${id}/check`)
-      .then(res => {
-        let todos = this.state.todos;
-        todos[index].checked = !todos[index].checked;
-        this.setState({todos});
-      });
+  check(id, index) {
+    axios.patch(`${api_url}/todo/${id}/check`).then((res) => {
+      let todos = this.state.todos;
+      todos[index].checked = !todos[index].checked;
+      this.setState({ todos });
+    });
   }
 
   componentDidMount() {
-    axios.get(`${api_url}/todos`)
-      .then(res => {
+    axios
+      .get(`${api_url}/todos`)
+      .then((res) => {
         const todos = res.data;
         this.setState({ todos: todos, loading: false });
+      })
+      .catch((err) => {
+        console.error(err);
       });
   }
 
-  changeShow(){
-    this.setState({show_done: !this.state.show_done});
+  changeShow() {
+    this.setState({ show_done: !this.state.show_done });
   }
 
   render() {
-    if(this.state.loading){
+    if (this.state.loading) {
       return (
         <Fragment>
           <div className="container">
@@ -67,41 +70,49 @@ class Home extends Component {
               <h3>Loading</h3>
             </div>
           </div>
-          <BottomNav path={this.props.location}/>
+          <BottomNav path={this.props.location} />
         </Fragment>
       );
     }
 
     let todos = this.state.todos
-      .filter((todo) => this.state.show_done ? true : !todo.checked)
+      .filter((todo) => (this.state.show_done ? true : !todo.checked))
       .map((todo, index) => {
-        return <TodoCard key={todo.id} todo={todo} delete={this.delete} check={this.check} index={this.state.todos.indexOf(todo)}/>
+        return (
+          <TodoCard
+            key={todo.id}
+            todo={todo}
+            delete={this.delete}
+            check={this.check}
+            index={this.state.todos.indexOf(todo)}
+          />
+        );
       });
 
     let count = {
       done: this.state.show_done ? todos.length : this.state.todos.length - todos.length,
       todo: this.state.show_done ? this.state.todos.length - todos.length : todos.length,
-    }
+    };
 
     return (
       <Fragment>
         <div className="container">
           <div>
-            <p style={{float: "right", margin: 0}}>
+            <p style={{ float: 'right', margin: 0 }}>
               {` todo: ${count.todo} (${count.done} done)`}
             </p>
 
             <label>
-              <input type="checkbox" checked={this.state.show_done} onChange={this.changeShow}/>
+              <input type="checkbox" checked={this.state.show_done} onChange={this.changeShow} />
               Show completed
             </label>
           </div>
 
-          { todos }
+          {todos}
 
-          <CreateTodoButton/>
+          <CreateTodoButton />
         </div>
-        <BottomNav path={this.props.location}/>
+        <BottomNav path={this.props.location} />
       </Fragment>
     );
   }
