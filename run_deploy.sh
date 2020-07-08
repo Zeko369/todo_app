752fe65eeafcbe017971dbb0c3d588d39c48ed49
Fran Zekan <zekan.fran369@gmail.com>
2020-07-08 17:41:25 +0200

    Fix deploy

--- run_deploy.sh
+++ run_deploy.sh
@@ -2,6 +2,7 @@
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
+git checkout .
 git pull origin master
 NODE_ENV=development yarn install
 NODE_ENV=development sequelize db:migrate
