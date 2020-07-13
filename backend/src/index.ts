9dac443f55e5430e22b3e787e0afba7f88a49b75
Fran Zekan <zekan.fran369@gmail.com>
2020-07-13 21:38:20 +0200

    Setup logging and cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,16 @@
 import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+
 import router from './old/todos';
 
 const PORT = process.env.PORT || 8080;
 const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use('/api/todos', router);
 
