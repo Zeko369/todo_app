4faa08b2fd8d6d4806cf090998ceae41f3a764ff
Fran Zekan <zekan.fran369@gmail.com>
2020-07-14 22:17:55 +0200

    Remove old backend

--- controllers/index.js
+++
@@ -1,9 +0,0 @@
-const TodosController = require('./todos_controller');
-const TasksController = require('./tasks_controller');
-
-console.log(TasksController);
-
-module.exports = {
-  TodosController,
-  TasksController,
-};
--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-git push
-ssh vps -t "cd todo_app && sh run_deploy.sh"
-echo "Done"
