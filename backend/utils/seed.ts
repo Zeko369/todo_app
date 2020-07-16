cfadbce35c27edbd03d9b51bb4c090b153bdc2ef
Fran Zekan <zekan.fran369@gmail.com>
2020-07-16 21:32:28 +0200

    New migrations

--- backend/utils/seed.ts
+++ backend/utils/seed.ts
@@ -5,14 +5,31 @@ import { lorem, date } from 'faker';
   const db = new PrismaClient();
   await db.connect();
 
+  const lists: number[] = [];
+
+  for (let i = 0; i < 3; i++) {
+    const list = await db.list.create({
+      data: {
+        title: lorem.words(3),
+      },
+    });
+
+    lists.push(list.id);
+  }
+
+  const randomListId = () => {
+    return lists[Math.floor(lists.length * Math.random())];
+  };
+
   for (let i = 0; i < 10; i++) {
     const checked = Math.random() > 0.5;
-    await db.todos.create({
+    await db.todo.create({
       data: {
         title: lorem.words(3),
         description: lorem.paragraph(),
         checked,
         checkedAt: checked ? date.past() : null,
+        list: { connect: { id: randomListId() } },
       },
     });
   }
