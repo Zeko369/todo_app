25386f80e8a6a49e3b45e56bd95f7cea59fc8023
Fran Zekan <zekan.fran369@gmail.com>
2020-07-18 21:00:40 +0200

    New backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,71 @@
-import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
+import { schema, use } from 'nexus';
+import { intArg } from '@nexus/schema';
+import { prisma } from 'nexus-plugin-prisma';
 
-import router from './old/todos';
+use(prisma({ migrations: true, features: { crud: true } }));
 
-const PORT = process.env.PORT || 8080;
-const app = express();
+schema.queryType({
+  definition(t) {
+    t.crud.todo();
+    t.crud.todos({ ordering: true });
 
-app.use(cors());
-app.use(morgan('dev'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+    t.crud.list();
+    t.crud.lists({ filtering: true });
+  },
+});
 
-app.use('/api/todos', router);
+schema.mutationType({
+  definition(t) {
+    t.crud.createOneTodo();
+    t.crud.updateOneTodo();
+    t.crud.deleteOneTodo();
+    t.field('checkTodo', {
+      type: 'Todo',
+      args: { id: intArg({ required: true }) },
+      resolve: async (parent, args, ctx) => {
+        const { id } = args;
 
-app.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}`);
+        const todo = await ctx.db.todo.findOne({ where: { id } });
+        if (todo) {
+          return ctx.db.todo.update({
+            where: { id },
+            data: { checked: !todo.checked, checkedAt: todo.checked ? null : new Date() },
+          });
+        }
+
+        return null;
+      },
+    });
+
+    t.crud.createOneList();
+    t.crud.updateOneList();
+    t.crud.deleteOneList();
+  },
+});
+
+schema.objectType({
+  name: 'Todo',
+  definition(t) {
+    t.model.id();
+    t.model.title();
+    t.model.description();
+    t.model.list();
+    t.model.checked();
+    t.model.checkedAt();
+    t.model.requires();
+    t.model.requiredBy();
+    t.model.createdAt();
+    t.model.updatedAt();
+  },
+});
+
+schema.objectType({
+  name: 'List',
+  definition(t) {
+    t.model.id();
+    t.model.title();
+    t.model.todos();
+    t.model.createdAt();
+    t.model.updatedAt();
+  },
 });
