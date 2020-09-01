c51405f0f1b35b8592adaab6975e1700f9088b97
Fran Zekan <zekan.fran369@gmail.com>
2020-09-01 14:04:40 +0200

    Add tagBased filtering

--- client/src/pages/lists/[id]/index.tsx
+++ client/src/pages/lists/[id]/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { NextPage } from 'next';
 import {
   Heading,
@@ -11,18 +11,30 @@ import {
   Stack,
   Text,
   Button,
+  TagIcon,
 } from '@chakra-ui/core';
 import { useRouter } from 'next/router';
 
-import { useListQuery, useCheckTodoMutation } from '../../../generated/graphql';
+import { useListQuery, useCheckTodoMutation, Tag as TagDB } from '../../../generated/graphql';
 import { getId } from '../../../helpers/getId';
 import Nav from '../../../components/Nav';
 import { TODOS_QUERY } from '../../../graphql/queries';
 import useSaveToggle from '../../../hooks/useSaveToggle';
 
+type Tag = Pick<TagDB, 'id' | 'text'>;
+
+const getTagIds = (tagIds: string | string[] | undefined): number[] => {
+  if (tagIds && !Array.isArray(tagIds)) {
+    return tagIds.split(',').map((id) => parseInt(id));
+  }
+
+  return [];
+};
+
 const ListPage: NextPage = () => {
   const router = useRouter();
   const id = getId(router.query) || -1;
+  const tagIds: number[] = getTagIds(router.query.tags);
 
   const { loading, error, data } = useListQuery({ variables: { id } });
   const [checkTodo] = useCheckTodoMutation({
@@ -33,6 +45,38 @@ const ListPage: NextPage = () => {
 
   const check = (id: number) => async () => {
     await checkTodo({ variables: { id } });
+  };
+
+  const tags = useMemo(() => {
+    if (!loading && !error && data && data.list) {
+      return data.list.todos.reduce((prev, todo) => {
+        const all = [...prev, ...todo.tags];
+        const out: Tag[] = [];
+
+        for (let i = 0; i < all.length; i++) {
+          if (!out.find((t) => t.id === all[i].id)) {
+            out.push(all[i]);
+          }
+        }
+
+        return out;
+      }, [] as Tag[]);
+    }
+
+    return [];
+  }, [loading, data, error]);
+
+  const selectTag = (tagId: number) => () => {
+    if (tagId === -1) {
+      return router.replace('/lists/[id]', `/lists/${id}`);
+    }
+
+    const newTags = (tagIds.includes(tagId)
+      ? tagIds.filter((a) => a !== tagId)
+      : [...tagIds, tagId]
+    ).join(',');
+
+    router.replace(`/lists/[id]?tags=${newTags}`, `/lists/${id}?tags=${newTags}`);
   };
 
   return (
@@ -47,23 +91,56 @@ const ListPage: NextPage = () => {
       ) : error || !data ? (
         <Heading size="xl">Error :(</Heading>
       ) : (
-        <Stack>
-          {data.list?.todos
-            .filter((todo) => showAll || !todo.checked)
-            .map((todo) => (
-              <Stack key={todo.id} isInline spacing={2} alignItems="center">
-                <Checkbox isChecked={todo.checked} onChange={check(todo.id)} />
-                <Text textDecoration={todo.checked ? 'line-through' : ''}>{todo.title}</Text>
-                {todo.tags.map((tag) => (
-                  <Tag key={tag.id} size="sm">
-                    <Flex alignItems="center">
-                      <TagLabel>{tag.text}</TagLabel>
-                    </Flex>
-                  </Tag>
-                ))}
-              </Stack>
-            ))}
-        </Stack>
+        <>
+          <Box mb={5}>
+            <Heading size="md" mb={2}>
+              Filter by tag:
+            </Heading>
+            <Stack isInline spacing={2}>
+              {tags.map((tag) => (
+                <Tag
+                  key={tag.id}
+                  size="sm"
+                  variantColor="blue"
+                  variant={tagIds.includes(tag.id) ? 'solid' : 'subtle'}
+                  as="button"
+                  onClick={selectTag(tag.id)}
+                >
+                  <Flex alignItems="center">
+                    <TagLabel>{tag.text}</TagLabel>
+                  </Flex>
+                </Tag>
+              ))}
+              <Tag size="sm" variantColor="blue" as="button" onClick={selectTag(-1)}>
+                <Flex alignItems="center">
+                  <TagLabel>NONE</TagLabel>
+                  <TagIcon icon="close" size="12px" />
+                </Flex>
+              </Tag>
+            </Stack>
+          </Box>
+          <Stack>
+            {data.list?.todos
+              .filter((todo) => showAll || !todo.checked)
+              .map((todo) => (
+                <Stack key={todo.id} isInline spacing={2} alignItems="center">
+                  <Checkbox isChecked={todo.checked} onChange={check(todo.id)} />
+                  <Text textDecoration={todo.checked ? 'line-through' : ''}>{todo.title}</Text>
+                  {todo.tags.map((tag) => (
+                    <Tag
+                      key={tag.id}
+                      size="sm"
+                      variant={tagIds.includes(tag.id) ? 'solid' : 'subtle'}
+                    >
+                      <Flex alignItems="center">
+                        <TagLabel>{tag.text}</TagLabel>
+                      </Flex>
+                    </Tag>
+                  ))}
+                </Stack>
+              ))}
+          </Stack>
+        </>
       )}
     </Box>
   );
