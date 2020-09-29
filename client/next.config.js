68425e5581cbc314e8b6ecc6eade0554ad3d4d45
Fran Zekan <zekan.fran369@gmail.com>
2020-09-30 01:54:53 +0200

    Add vim jk->Esc

--- client/next.config.js
+++ client/next.config.js
@@ -1,15 +1,13 @@
 // @ts-check
 
-const compose = require('next-compose-plugins');
 const withCSS = require('@zeit/next-css');
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = compose([
-  withCSS(),
+module.exports = withCSS(
   withBundleAnalyzer({
     trailingSlash: false,
-  }),
-]);
+  })
+);
