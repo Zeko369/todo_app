d8e3f99f8c81d449b7149964c27daa2553c2d5d4
Fran Zekan <zekan.fran369@gmail.com>
2020-09-30 01:05:25 +0200

    Refactor form out of new

---
+++ client/src/modules/Notes/components/NoteForm.tsx
@@ -0,0 +1,66 @@
+import React, { useRef, useState } from 'react';
+import { Stack, Flex, Button, FormControl, FormLabel, Spinner } from '@chakra-ui/core';
+import Nav from '../../../components/Nav';
+import Editor, { EditorFunctions } from './Editor';
+import Input from '../../../components/Input';
+
+interface NoteForm {
+  onClick: (title: string, code: string) => Promise<void>;
+  initValues?: {
+    title: string;
+    code?: string;
+  } | null;
+}
+
+const initCode = 'Hello';
+
+export const NoteForm: React.FC<NoteForm> = (props) => {
+  const ref = useRef<EditorFunctions>(null);
+  const [title, setTitle] = useState(props.initValues?.title || '');
+
+  const onClick = async () => {
+    const code = ref.current?.getCode();
+
+    if (code) {
+      await props.onClick(title, code);
+    }
+  };
+
+  return (
+    <Stack w="90%" maxW="1000px" m="0 auto" h="100vh">
+      <Nav />
+      {props.children}
+      {props.initValues === null ? (
+        <Spinner />
+      ) : (
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            onClick();
+          }}
+        >
+          <Flex justify="space-between" alignItems="flex-end">
+            <Input
+              name="Name"
+              outerProps={{ w: '100%', mr: 2 }}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTitle(e.target.value)}
+              value={title}
+            />
+            <Button onClick={onClick} variantColor="green">
+              Save
+            </Button>
+          </Flex>
+        </form>
+      )}
+
+      {props.initValues === null ? (
+        <Spinner />
+      ) : (
+        <FormControl isRequired>
+          <FormLabel>Content</FormLabel>
+          <Editor ref={ref} initCode={props.initValues?.code || initCode} language="markdown" />
+        </FormControl>
+      )}
+    </Stack>
+  );
+};
--- client/src/modules/Notes/pages/NewNote.tsx
+++ client/src/modules/Notes/pages/NewNote.tsx
@@ -1,25 +1,15 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 import { useRouter } from 'next/router';
 import { NextPage } from 'next';
-import { Stack, Button, Heading, Flex, FormControl, FormLabel } from '@chakra-ui/core';
-import Editor, { EditorFunctions } from '../components/Editor';
-import Input from '../../../components/Input';
-import Nav from '../../../components/Nav';
 import { useCreateNoteMutation } from '../../../generated/graphql';
 import { NOTES_QUERY } from '../graphql/queries';
-
-const initCode = '# Hello world\n\n- foo\n- bar\n\n```javascript\nconsole.log(`Hello world`);\n```';
+import { NoteForm } from '../components/NoteForm';
 
 export const NewNotePage: NextPage = () => {
   const router = useRouter();
-
-  const ref = useRef<EditorFunctions>(null);
-  const [title, setTitle] = useState('');
   const [createNote] = useCreateNoteMutation({ refetchQueries: [{ query: NOTES_QUERY }] });
 
-  const onClick = async () => {
-    const code = ref.current?.getCode();
-
+  const onClick = async (title: string, code: string) => {
     if (!code) return;
 
     const res = await createNote({ variables: { title, description: code } });
@@ -30,26 +20,5 @@ export const NewNotePage: NextPage = () => {
     }
   };
 
-  return (
-    <Stack w="90%" maxW="1000px" m="0 auto" h="100vh">
-      <Nav />
-      <Heading>New note</Heading>
-      <Flex justify="space-between" alignItems="flex-end">
-        <Input
-          name="Name"
-          outerProps={{ w: '100%', mr: 2 }}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTitle(e.target.value)}
-          value={title}
-        />
-        <Button onClick={onClick} variantColor="green">
-          Save
-        </Button>
-      </Flex>
-
-      <FormControl isRequired>
-        <FormLabel>Content</FormLabel>
-        <Editor ref={ref} initCode={initCode} language="markdown" />
-      </FormControl>
-    </Stack>
-  );
+  return <NoteForm onClick={onClick} />;
 };
