2c5ccc31e1de6f276f2b9e2a1b9f591ce378f833
Fran Zekan <zekan.fran369@gmail.com>
2020-09-30 02:15:11 +0200

    Make notes actual list

--- client/src/modules/Notes/pages/Notes.tsx
+++ client/src/modules/Notes/pages/Notes.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NextPage } from 'next';
-import { Box, Spinner, Heading } from '@chakra-ui/core';
+import { Box, Spinner, Heading, List, ListItem } from '@chakra-ui/core';
 import { Link, LinkButton } from 'chakra-next-link';
 
 import Nav from '../../../components/Nav';
@@ -14,51 +14,20 @@ export const NotesPage: NextPage = () => {
       <Nav>
         <LinkButton href="/notes/new">New</LinkButton>
       </Nav>
-      {/* {showNew && (
-        <Box mb="5">
-          <Flex justifyContent="space-between" align="center">
-            <Heading>{editingId === -1 ? 'Add new tag' : `Editing tag-[${editingId}]`}</Heading>
-            {editingId !== -1 && (
-              <CloseButton
-                onClick={() => {
-                  setEditingId(-1);
-                  setValue('text', '');
-                }}
-              />
-            )}
-          </Flex>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <Stack spacing={3}>
-              <Input name="text" ref={register({ required: true })} isRequired />
-              <Select
-                ref={register({ required: true })}
-                isRequired
-                name="color"
-                bg={`${color}.100`}
-              >
-                {colors.map((color) => (
-                  <option value={color}>{color}</option>
-                ))}
-              </Select>
-              <Button type="submit">{editingId === -1 ? 'Create' : 'Update'}</Button>
-            </Stack>
-          </form>
-        </Box>
-      )} */}
       {loading ? (
         <Spinner />
       ) : error || !data ? (
         <Heading size="xl">Error :(</Heading>
       ) : (
-        <Box>
+        <List styleType="disc">
           {data.todos.map((note) => (
-            <Box>
+            <ListItem>
               <Link href="/notes/[id]" nextAs={`/notes/${note.id}`}>
                 {note.title}
               </Link>
-            </Box>
+            </ListItem>
           ))}
-        </Box>
+        </List>
       )}
     </Box>
   );
