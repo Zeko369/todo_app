9804e68340315f8974575dd93d002f1640fe9de4
Fran Zekan <zekan.fran369@gmail.com>
2020-12-28 20:55:11 +0100

    Add dark toggle on auth

--- client/src/modules/Auth/components/AuthLayout.tsx
+++ client/src/modules/Auth/components/AuthLayout.tsx
@@ -1,12 +1,20 @@
-import { Box, Heading, useColorMode } from '@chakra-ui/core';
+import { Box, Heading, IconButton, useColorMode } from '@chakra-ui/core';
 import React from 'react';
 
 export const AuthLayout: React.FC = ({ children }) => {
-  const { colorMode } = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode();
   const light = colorMode === 'light';
 
   return (
     <Box h="100vh" w="100wv" bg={light ? 'blue.300' : 'blue.600'}>
+      <IconButton
+        mr="2"
+        mt="2"
+        float="right"
+        icon={colorMode === 'dark' ? 'moon' : 'sun'}
+        aria-label={colorMode === 'dark' ? 'light' : 'dark'}
+        onClick={toggleColorMode}
+      />
       <Box
         pos="absolute"
         top="30vh"
