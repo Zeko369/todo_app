51cd7a157f4018af28234c98fbbf61efaf568166
Fran Zekan <zekan.fran369@gmail.com>
2020-12-29 00:22:09 +0100

    Fix yarn not in path

--- scripts/postdeploy.sh
+++ scripts/postdeploy.sh
@@ -1,10 +1,9 @@
-#!/bin/sh
+#!/bin/zsh
 
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
-echo $(whoami)
-echo $(pwd)
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 
 cp ~/envs/todo ./backend/prisma/.env
 cp ~/envs/todo_client ./client/.env
