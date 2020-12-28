3f8d8a8c6a545e3e2841959b38073bb114fe0653
Fran Zekan <zekan.fran369@gmail.com>
2020-12-29 00:16:56 +0100

    Fix .env names

--- scripts/postdeploy.sh
+++ scripts/postdeploy.sh
@@ -3,8 +3,8 @@
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
-cp ~/envs/.todo .env
-cp ~/envs/.todo_client ./client/.env
+cp ~/envs/todo .env
+cp ~/envs/todo_client ./client/.env
 
 yarn
 
