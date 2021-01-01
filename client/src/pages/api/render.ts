53533cb9dd378f99243f512ce6335e858a3336a4
Fran Zekan <zekan.fran369@gmail.com>
2021-01-02 00:34:53 +0100

    Fix MDX rendering

--- client/src/pages/api/render.ts
+++ client/src/pages/api/render.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import renderToString from 'next-mdx-remote/render-to-string';
-// import mdxPrism from 'mdx-prism';
-// import remarkCodeTitles from 'remark-code-titles';
+import mdxPrism from 'mdx-prism';
+import remarkCodeTitles from 'remark-code-titles';
 import { components } from '../../components/md';
 
 const RenderMDX = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -13,10 +13,10 @@ const RenderMDX = async (req: NextApiRequest, res: NextApiResponse) => {
 
   const mdxSource = await renderToString(source, {
     components,
-    // mdxOptions: {
-    //   remarkPlugins: [remarkCodeTitles],
-    //   rehypePlugins: [mdxPrism],
-    // },
+    mdxOptions: {
+      remarkPlugins: [remarkCodeTitles],
+      rehypePlugins: [mdxPrism],
+    },
   });
 
   res.json({ mdxSource });
